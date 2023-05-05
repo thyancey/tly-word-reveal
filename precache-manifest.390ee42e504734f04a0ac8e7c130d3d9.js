8e0a07990a844b4a9a0bb33f6d85b8a1941c0554
Tom Yancey <thyancey84@gmail.com>
2023-05-04 23:40:19 -0400

    Updates

---
+++ precache-manifest.390ee42e504734f04a0ac8e7c130d3d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cd47c3714532754ebfe5eb0cd97ce4d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d4e88b5164993465a901",
+    "url": "./static/css/main.b638cb3e.chunk.css"
+  },
+  {
+    "revision": "b0a189448e2fa7cf3fd0",
+    "url": "./static/js/2.ba29f78e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.ba29f78e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4e88b5164993465a901",
+    "url": "./static/js/main.8869937a.chunk.js"
+  },
+  {
+    "revision": "c7552e571a20efa368b1",
+    "url": "./static/js/runtime-main.6ffcbb0a.js"
+  }
+]);
